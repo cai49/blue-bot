4ce0755b163f85d8059d4b9f0842a56814c56cce
CAI_4 <sunsetcity93@gmail.com>
2025-04-24 01:46:04 -0700

    Update Automated Install Script

--- install_requirements.bat
+++ install_requirements.bat
@@ -1,6 +1,7 @@
 @echo "Installing Requirements"
 
-python -m venv .\BlueBot > $null
-.\Scripts\activate
-pip install -r requirements.txt > $null
-deactivate
+call git clone https://github.com/cai49/blue-bot.git
+call python -m venv .\blue-bot\
+call cd .\blue-bot\
+call .\Scripts\activate
+call pip install -r .\requirements.txt
